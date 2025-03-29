ca611b9f75665a4afbb959c5a8b898a2e6062cd7
N0m@d-Xii <hugewolf1996@gmail.com>
2025-03-29 16:33:03 +0700

    เพิ่มแจ้งเตือนในการเริ่มปริ้น

---
+++ backend/sendmsg.php
@@ -0,0 +1,19 @@
+<?php 
+require_once 'loadEnv.php';
+loadEnv('../.env');
+
+$data =json_decode(file_get_contents('php://input'),true);
+
+if(empty($data['text'])){
+   echo "ไม่สามารถส่งข้อมูลได้";
+   exit();
+}
+
+$Bot_Token = $_ENV['BOT_TOKEN'];
+$Chat_ID = $_ENV['CHAT_ID'];
+
+$url = "https://api.telegram.org/bot$Bot_Token/sendMessage?chat_id=$Chat_ID&text=$data[text]";
+
+$result = file_get_contents($url, false);
+echo $result;
+?>
---
+++ js/scan.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function() {        
+    LaunchUrl();
+    startCamera();
+});
+
+    function startCamera() {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+            scanQRCode();
+        })
+        .catch(err => {
+            qrResult.innerText = "🚨 ไม่สามารถเปิดกล้องได้: " + err;
+        });
+    }
+
+   async function LaunchUrl() {
+        const params = new URLSearchParams(window.location.search);
+        const msg = params.get("text"); // สมมติว่าต้องการส่งค่า page
+        let url_text = encodeURIComponent(msg+" เริ่มปริ้น\n"+new Date().toLocaleString("th-TH"));
+        const jobname = document.getElementById("jobname");
+        jobname.innerHTML = msg;
+
+        try {
+            const response = await fetch('./backend/sendmsg.php',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: JSON.stringify({text : url_text})
+            });
+
+            if (response.ok) {
+                console.log('Data sent successfully');
+            }
+            
+        } catch (error) {
+            console.error('Error fetching data:'+ error);
+        }
+    }
+
+    function scanQRCode() {
+    const video = document.getElementById("video");
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    const qrResult = document.getElementById("qrResult");
+    // ดึงค่าจาก URL ปัจจุบัน
+    const params = new URLSearchParams(window.location.search);
+    const page = params.get("text"); // สมมติว่าต้องการส่งค่า page
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            let code = jsQR(imageData.data, imageData.width, imageData.height);
+
+            if (code) {
+                let qrText = code.data;
+
+                // เช็คว่า URL ถูกต้องหรือไม่
+                if (qrText.startsWith("https://api.telegram.org")) {
+                    // console.log("✅ ลิงก์ Telegram ถูกต้อง กำลังเปิด...");
+                    qrResult.innerHTML = `✅ พบ QR Code: <br> <a href="${qrText}" target="_blank">${qrText+page}</a>`;
+                    window.open("result.html?url="+qrText+page+" ปริ้นเสร็จแล้ว \n"+new Date().toLocaleString("th-TH"));
+                    setTimeout(() => {
+                        window.location.href = "index.html"; 
+                    }, 1000);
+                } else {
+                    console.warn("⚠️ QR Code ไม่ใช่ Telegram URL:", qrText);
+                }
+                return;
+            }
+        }
+        requestAnimationFrame(scanQRCode); // Loop การสแกน
+    }
